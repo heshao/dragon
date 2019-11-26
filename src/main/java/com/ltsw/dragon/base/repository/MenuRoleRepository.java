617e2db8f24f1d9cbfc99eb5c45c442a7403f6df
heshao <553555561@qq.com>
2019-11-26 14:23:34 +0800

    删除菜单级联删除关系异常

--- src/main/java/com/ltsw/dragon/base/repository/MenuRoleRepository.java
+++ src/main/java/com/ltsw/dragon/base/repository/MenuRoleRepository.java
@@ -3,7 +3,10 @@ package com.ltsw.dragon.base.repository;
 import com.ltsw.dragon.base.entity.MenuRole;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -28,7 +31,7 @@ public interface MenuRoleRepository extends JpaRepository<MenuRole, Long> {
      * @param menuId 菜单Id
      * @return
      */
-    List<MenuRole> findByMenu_Id(long menuId);
+    List<MenuRole> findByMenuId(long menuId);
 
     /**
      * 查询菜单角色关系
@@ -36,19 +39,25 @@ public interface MenuRoleRepository extends JpaRepository<MenuRole, Long> {
      * @param roleId 角色Id
      * @return
      */
-    List<MenuRole> findByRole_Id(long roleId);
+    List<MenuRole> findByRoleId(long roleId);
 
     /**
      * 删除关系
      *
      * @param menuId 菜单ID
      */
-    void deleteByMenu_Id(long menuId);
+    @Transactional(rollbackFor = Exception.class)
+    @Modifying
+    @Query(value = "delete from menu_role where menu_id = ?1 ", nativeQuery = true)
+    void deleteByMenuId(long menuId);
 
     /**
      * 删除关系
      *
      * @param roleId 角色ID
      */
-    void deleteByRole_Id(long roleId);
+    @Transactional(rollbackFor = Exception.class)
+    @Modifying
+    @Query(value = "delete from menu_role where role_id = ?1 ", nativeQuery = true)
+    void deleteByRoleId(Long roleId);
 }
